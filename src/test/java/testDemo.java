5362b5b2008351229804b779fa1dfb0cd7c55f50
m0vntain <jmcdador@gmail.com>
2018-07-30 14:10:32 +0800

    println on DEV branch

--- src/test/java/testDemo.java
+++ src/test/java/testDemo.java
@@ -1,7 +1,6 @@
 public class testDemo {
 
     public static void main(String []args){
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("Created on DEV branch");
     }
 }
